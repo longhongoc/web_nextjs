439994158516569dab3a09427302990a82306dc7
longbin25 <hongoclong250802@gmail.com>
2025-09-27 09:02:24 +0700

    added above commit, version 2

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,10 +1,10 @@
-import {defineRouting} from 'next-intl/routing';
- 
+import { defineRouting } from 'next-intl/routing';
+
 export const routing = defineRouting({
   // A list of all locales that are supported
   locales: ['en', 'ja'],
-  localePrefix: "as-needed",
- 
+  localePrefix: 'as-needed',
+
   // Used when no locale matches
-  defaultLocale: 'en'
+  defaultLocale: 'en',
 });
