1b27ac31e98b6eb42ae9e28dc63dec772921aedd
longbin25 <hongoclong250802@gmail.com>
2025-09-28 14:50:44 +0700

    remove DELETE function in api/restration

--- src/app/api/registration/route.ts
+++ src/app/api/registration/route.ts
@@ -48,23 +48,3 @@ export async function POST(req: Request) {
     );
   }
 }
-
-export async function DELETE(
-  req: Request,
-  { params }: { params: { id: string } }
-) {
-  try {
-    const userId = params.id;
-
-    // Xoá user theo id
-    await sql`DELETE FROM registration_infor WHERE id = ${userId}`;
-
-    return NextResponse.json({ message: 'User deleted successfully' });
-  } catch (err) {
-    console.error(err);
-    return NextResponse.json(
-      { error: 'Failed to delete user' },
-      { status: 500 }
-    );
-  }
-}
